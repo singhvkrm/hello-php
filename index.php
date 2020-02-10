02a13ee700516c9fb70fb335129e732246c69e49
singhvkrm <singh.vkrm@gmail.com>
2020-02-10 22:55:18 +0100

    chg1

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>Hello</title>
  </head>
  <body>
- <?php echo '<center><font size="28">Hello OpenShift 2018@Statoil! </font> <br><h5>powered by Red Hat</h5></center>'; ?> 
+ <?php echo '<center><font size="28">Hello OpenShift 2020@DnB! </font> <br><h5>powered by Red Hat</h5></center>'; ?> 
  </body>
 </html>
